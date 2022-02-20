08ee2682b4a41642fe4080637c8217bb031ef7ec
ZeroStudio3645 <77138982+ZeroStudio3645@users.noreply.github.com>
2022-02-20 11:17:05 +0700

    Add files via upload

---
+++ script2.sh
@@ -0,0 +1,11 @@
+add-apt-repository ppa:papirus/papirus
+apt update
+apt install \
+    gnome-tweaks \
+    chrome-gnome-shell \
+    papirus-icon-theme \
+    dmz-colour-theme \
+    gnome-themes-extra \ 
+    fonts-cantarell \
+    gnome-shell-extension-appindicator
+apt install gnome-software
